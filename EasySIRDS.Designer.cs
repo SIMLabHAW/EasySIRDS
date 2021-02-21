fe56e0779427c03cd111249e17fe89549d70e748
niklasschwetke <77964955+niklasschwetke@users.noreply.github.com>
2021-02-21 22:23:36 +0100

    Add files via upload

---
+++ EasySIRDS.Designer.cs
@@ -0,0 +1,111 @@
+﻿/*
+ This file is part of EasySIRDS.
+
+    EasySIRDS is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    EasySIRDS is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with EasySIRDS.  If not, see<http://www.gnu.org/licenses/>.
+*/
+namespace SVRM
+{
+    partial class EasySIRDS
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(37, 29);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "change map source";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(37, 120);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(123, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "save picture";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(226, 177);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "SIRDS";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ EasySIRDS.cs
@@ -0,0 +1,162 @@
+﻿/*
+ This file is part of EasySIRDS.
+
+    EasySIRDS is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    EasySIRDS is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with EasySIRDS.  If not, see<http://www.gnu.org/licenses/>.
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SVRM
+{
+    public partial class EasySIRDS : Form
+    {
+        Bitmap map = null;
+
+        Bitmap randomImage = new Bitmap(128, 128); //rndwidth x rndheight
+        Bitmap bigrandom = new Bitmap(1024, 1024); //width x height
+
+        public int width = 1024; //width of final sirds
+        public int height = 1024; //height of final sirds
+
+        public int rndwidth = 128; //width of small pattern
+        public int rndheight = 128; //height of small pattern
+
+        public int xdpi = 75;
+        public int ydpi = 75;
+
+        public int distance = 25; //in cm
+        public double eyesep= 6.25; //in cm
+        public double mu = 0.25; //change for difference in depth, should be smaller than 1
+        public double z;
+        public int sep;
+
+        public string openpath;
+        public string savepath;
+        
+
+        public EasySIRDS()
+        {
+            InitializeComponent();
+
+            Text = "SIRDS";
+        }
+
+        public void algo(int width, int height)
+        {
+            int x2 = 0;
+            int y2 = 0;
+            Color big = Color.FromArgb(0,0,0);
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    Color zvalue = map.GetPixel(x,y);
+
+                    z = ((zvalue.R / (double)255.0)) * (mu * distance);
+
+                    sep = (int)(((1 - mu * z) / (2 - mu * z)) * eyesep);
+
+                    if (x < rndwidth)
+                    {
+                        if (y < height)
+                        {
+                            x2 = x % (rndwidth + sep);
+                            y2 = y % rndwidth;
+
+                            if (x2 < 0)
+                            {
+                                x2 = (rndwidth) + x2;
+                            }
+
+                            big = randomImage.GetPixel(x2, y2);
+                        }
+                    }
+
+                    else
+                    {
+                        x2 = x - (rndwidth + sep);
+                        y2 = y;
+
+                        if (x2 < 0)
+                        {
+                            x2 = (rndwidth) + x2;
+                        }
+
+                        big = bigrandom.GetPixel(x2, y2);
+                        
+                    }
+
+                    bigrandom.SetPixel(x, y, big);
+
+                }
+            }            
+        }
+
+        public void createrandomimage(int rndwidth, int rndheight)
+        {
+            Random rnd = new Random();
+
+            for (int x = 0; x < rndwidth; x++)
+            {
+                for (int y = 0; y < rndheight; y++)
+                {
+                    
+                    int rnd2 = rnd.Next(0,255);
+
+                    Color random = Color.FromArgb(rnd2, rnd2, rnd2);
+                    randomImage.SetPixel(x, y, random);
+                }
+            }            
+        }
+
+        private void button1_Click(object sender, EventArgs e) // generate
+        {        
+            using (var image = new Bitmap(openpath))
+            {
+                map = new Bitmap(image);
+            }
+
+            createrandomimage(rndwidth, rndheight);
+
+            algo(width, height);
+        }
+
+        private void button2_Click(object sender, EventArgs e) //change source
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) 
+            { 
+                openpath = openFileDialog1.FileName;
+            }
+        }
+        private void button3_Click(object sender, EventArgs e) //change saving location
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            { 
+                savepath = saveFileDialog1.FileName;
+            }
+
+            bigrandom.Save(savepath + ".bmp");
+            randomImage.Save(savepath + "_pattern.bmp");
+
+        }
+    }
+}
